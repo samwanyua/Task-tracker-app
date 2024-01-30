fe17b9d059cbbd6dbf2f1c43b494f8ebe9bcc5be
samwanyua <samexample8@gmail.com>
2024-01-30 20:45:48 +0300

    Updated Parent file - added Optional message when there're no tasks

--- task-app/src/App.js
+++ task-app/src/App.js
@@ -34,7 +34,7 @@ function App() {
   return (
     <div className="container"> 
       <Header />
-      <Task tasks ={tasks} onDelete={deleteTask}/>
+      {tasks.length > 0 ? <Task tasks ={tasks} onDelete={deleteTask}/> : 'No tasks available!'}
     </div>
   );
 }
