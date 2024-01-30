46e38bc1155078ac1bc32ccc0a4b1a4615396dba
samwanyua <samexample8@gmail.com>
2024-01-30 21:05:09 +0300

    Updated  Tasks

--- task-app/src/Components/Task.js
+++ task-app/src/Components/Task.js
@@ -1,12 +1,12 @@
 import Role from "./Role"
 
 
-const Task = ({tasks, onDelete}) => {
+const Task = ({tasks, onDelete, onToggle}) => {
     
   return (
     <>
     {tasks.map((task) =>(
-        <Role key={task.id} task = {task} onDelete={onDelete}/>
+        <Role key={task.id} task = {task} onDelete={onDelete} onToggle={onToggle}/>
     ))}
     </>
   )
