260db695724039d1554c0a24903486bb798cee56
samwanyua <samexample8@gmail.com>
2024-01-30 20:00:50 +0300

    Updated  Tasks

--- task-app/src/Components/Task.js
+++ task-app/src/Components/Task.js
@@ -1,30 +1,12 @@
-const tasks = [
-    {
-        id: 1,
-        text: 'Doctors appointment',
-        day: 'Feb 5th at 2:30pm',
-        reminder: true,
-    },
-    {
-        id: 2,
-        text: 'Meeting at school',
-        day: 'Feb 6th at 1:30pm',
-        reminder: true,
-    },
-    {
-        id: 3,
-        text: 'Food shopping',
-        day: 'Feb 5th at 2:30pm',
-        reminder: false,
-    }
-]
+import Role from "./Role"
 
 
-const Task = () => {
+const Task = ({tasks}) => {
+    
   return (
     <>
     {tasks.map((task) =>(
-        <h3 key={task.id}>{task.text}</h3>
+        <Role key={task.id} task = {task}/>
     ))}
     </>
   )
