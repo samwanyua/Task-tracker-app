7c118251d5d96d95ec02c0c855aef4b206577ed9
samwanyua <samexample8@gmail.com>
2024-01-30 18:51:53 +0300

    Added Props, propTypes, styling, Button components

---
+++ task-app/src/App.js
@@ -0,0 +1,15 @@
+import Header from "./Components/Header";
+
+
+function App() {
+  return (
+    <div className="container"> 
+      <Header />
+    </div>
+  );
+}
+
+
+
+export default App;
+// empty angle brackets --> fragments
---
+++ task-app/src/Components/Header.js
@@ -0,0 +1,27 @@
+import PropTypes from 'prop-types'
+import Button from './Button'
+const Header = ({title}) => {
+  return (
+    <header className='header'>
+        <h1 >{title}</h1>
+        <Button color='green' text='Add'/>
+    </header>
+  )
+}
+Header.defaultProps = {
+    title: 'Task Tracker',
+  }
+
+Header.propTypes ={
+    title: PropTypes.string.isRequired,
+}
+
+const headingStyle = {
+    color: 'yellow',
+    backgroundColor: 'black',
+    padding: '20px',
+    textAlign: 'center'
+
+}
+
+export default Header
