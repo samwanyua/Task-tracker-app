9e3afbc80c6a5329078f2e7d0fa756fa8f818417
samwanyua <samexample8@gmail.com>
2024-01-30 21:05:27 +0300

    Updated Task component for each Task

--- task-app/src/Components/Role.js
+++ task-app/src/Components/Role.js
@@ -1,9 +1,9 @@
 import {FaTimes} from 'react-icons/fa'
 
 
-const Role = ({task,onDelete}) => {
+const Role = ({task,onDelete, onToggle}) => {
   return (
-    <div className="task">
+    <div className="task" onDoubleClick={() => onToggle(task.id)}>
         <h3>
             {task.text} <FaTimes style={
                 {color: 'red', cursor: 'pointer'}} onClick ={() => onDelete(task.id)}/>
