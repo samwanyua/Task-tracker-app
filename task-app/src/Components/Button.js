42ada35f4fcdb4326f34d8057554d609869a04c2
samwanyua <samexample8@gmail.com>
2024-01-30 19:27:38 +0300

    Added event

--- task-app/src/Components/Button.js
+++ task-app/src/Components/Button.js
@@ -1,7 +1,13 @@
 import PropTypes from 'prop-types'
 
-const Button = ({color, text}) => {
-  return <button style={{backgroundColor: color}} className='btn'>{text}</button>
+const Button = ({color, text,onClick}) => {
+  
+  return (
+    <button onClick={onClick} style={{
+    backgroundColor: color}} className='btn'>
+        {text}
+    </button>
+ )
 
 }
 
@@ -12,6 +18,7 @@ Button.defaultProps = {
 Button.propTypes = {
     text: PropTypes.string,
     color: PropTypes.string,
+    onClick: PropTypes.func.isRequired,
 }
 
 export default Button
