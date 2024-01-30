df6716fe20690f51e1c29f2a83d531c9d1e75539
samwanyua <samexample8@gmail.com>
2024-01-30 19:40:52 +0300

    Added Tasks

---
+++ task-app/src/Components/Task.js
@@ -0,0 +1,33 @@
+const tasks = [
+    {
+        id: 1,
+        text: 'Doctors appointment',
+        day: 'Feb 5th at 2:30pm',
+        reminder: true,
+    },
+    {
+        id: 2,
+        text: 'Meeting at school',
+        day: 'Feb 6th at 1:30pm',
+        reminder: true,
+    },
+    {
+        id: 3,
+        text: 'Food shopping',
+        day: 'Feb 5th at 2:30pm',
+        reminder: false,
+    }
+]
+
+
+const Task = () => {
+  return (
+    <>
+    {tasks.map((task) =>(
+        <h3 key={task.id}>{task.text}</h3>
+    ))}
+    </>
+  )
+}
+
+export default Task
