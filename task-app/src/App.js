b4219f3a63f6899bb9e72998749dce09bd775ccc
samwanyua <samexample8@gmail.com>
2024-01-30 20:00:18 +0300

    Updated Parent file

--- task-app/src/App.js
+++ task-app/src/App.js
@@ -1,12 +1,33 @@
 import Header from "./Components/Header";
 import Task from "./Components/Task";
-
+import { useState } from "react"
 
 function App() {
+  const [tasks, setTasks ] = useState(
+    [
+        {
+            id: 1,
+            text: 'Doctors appointment',
+            day: 'Feb 5th at 2:30pm',
+            reminder: true,
+        },
+        {
+            id: 2,
+            text: 'Meeting at school',
+            day: 'Feb 6th at 1:30pm',
+            reminder: true,
+        },
+        {
+            id: 3,
+            text: 'Food shopping',
+            day: 'Feb 5th at 2:30pm',
+            reminder: false,
+        }
+    ])
   return (
     <div className="container"> 
       <Header />
-      <Task />
+      <Task tasks ={tasks}/>
     </div>
   );
 }
